b316d4ecc5cabc772d83c32de9027935b3b76d4f
Jon O'Connell <jjoc0205@gmail.com>
2023-02-21 18:28:41 -0500

    Worked on question 2

---
+++ first_exercise/forks.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(){
+    int status;
+    int pid = fork();
+    if(pid == 0){
+        printf("I am the grandchild (%i)\n", pid);
+        return 0;
+    }
+    //pid = fork();
+    else if(pid > 0){
+        printf("I am the child");
+    }
+    printf("I am the parent");
+    return 0;
+}
