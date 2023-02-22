efd073d225792fb8b73ba6a4a5e99c2704c5b992
Jon O'Connell <jjoc0205@gmail.com>
2023-02-22 09:21:03 -0500

    Added hw1

--- first_exercise/forks.c
+++ first_exercise/forks.c
@@ -5,16 +5,18 @@
 #include <unistd.h>
 
 int main(){
-    int status;
-    int pid = fork();
-    if(pid == 0){
-        printf("I am the grandchild (%i)\n", pid);
-        return 0;
-    }
-    //pid = fork();
-    else if(pid > 0){
-        printf("I am the child");
-    }
-    printf("I am the parent");
+//     int status;
+//     int pid = fork();
+//     if(pid == 0){
+//         printf("I am the grandchild (%i)\n", pid);
+//         return 0;
+//     }
+//     //pid = fork();
+//     else if(pid > 0){
+//         printf("I am the child");
+//     }
+//     printf("I am the parent");
+//####################################
+//####################################
     return 0;
 }
